8e32b2573dcaaa75bd54132dacae06d257ed0890
msqtan1107@gmail.com <msqtan1107@gmail.com>
2023-10-01 23:13:30 +0800

    initial commit

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    uint public minimumAge = 18; // Minimum age required
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
@@ -15,32 +14,39 @@ contract Assessment {
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns(uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
 
-        // make sure this is the owner
+        // Ensure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
 
-        // perform transaction
+        // Check age before allowing the deposit
+        require(checkAge(msg.sender), "You must be at least 18 years old to deposit");
+
+        // Perform transaction
         balance += _amount;
 
-        // assert transaction completed successfully
+        // Assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
 
-        // emit the event
+        // Emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
+    // Custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
         uint _previousBalance = balance;
+
+        // Check age before allowing the withdrawal
+        require(checkAge(msg.sender), "You must be at least 18 years old to withdraw");
+
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
@@ -48,13 +54,22 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
+        // Withdraw the given amount
         balance -= _withdrawAmount;
 
-        // assert the balance is correct
+        // Assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
 
-        // emit the event
+        // Emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function setMinimumAge(uint _age) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        minimumAge = _age;
+    }
+
+    function checkAge(address _user) internal view returns (bool) {
+        return minimumAge <= 18;
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import {useState, useEffect} from "react";
-import {ethers} from "ethers";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
+
+const ethToPhpRate = 95566.56;
 
 export default function HomePage() {
   const [ethWallet, setEthWallet] = useState(undefined);
@@ -11,36 +13,38 @@ export default function HomePage() {
   const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   const atmABI = atm_abi.abi;
 
-  const getWallet = async() => {
+  const [userAge, setUserAge] = useState(undefined);
+  const [balanceInPhp, setBalanceInPhp] = useState(undefined);
+
+  const getWallet = async () => {
     if (window.ethereum) {
       setEthWallet(window.ethereum);
     }
 
     if (ethWallet) {
-      const account = await ethWallet.request({method: "eth_accounts"});
+      const account = await ethWallet.request({ method: "eth_accounts" });
       handleAccount(account);
     }
-  }
+  };
 
   const handleAccount = (account) => {
     if (account) {
-      console.log ("Account connected: ", account);
+      console.log("Account connected: ", account);
       setAccount(account);
-    }
-    else {
+    } else {
       console.log("No account found");
     }
-  }
+  };
 
-  const connectAccount = async() => {
+  const connectAccount = async () => {
     if (!ethWallet) {
-      alert('MetaMask wallet is required to connect');
+      alert("MetaMask wallet is required to connect");
       return;
     }
-  
-    const accounts = await ethWallet.request({ method: 'eth_requestAccounts' });
+
+    const accounts = await ethWallet.request({ method: "eth_requestAccounts" });
     handleAccount(accounts);
-    
+
     // once wallet is set we can get a reference to our deployed contract
     getATMContract();
   };
@@ -49,69 +53,158 @@ export default function HomePage() {
     const provider = new ethers.providers.Web3Provider(ethWallet);
     const signer = provider.getSigner();
     const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
- 
+
     setATM(atmContract);
-  }
+  };
 
-  const getBalance = async() => {
+  const getBalance = async () => {
     if (atm) {
-      setBalance((await atm.getBalance()).toNumber());
-    }
-  }
+      const ethBalance = (await atm.getBalance()).toNumber();
+      setBalance(ethBalance);
 
-  const deposit = async() => {
+      // Convert ETH to PHP
+      const phpBalance = (ethBalance * ethToPhpRate).toFixed(2);
+      setBalanceInPhp(phpBalance);
+    }
+  };
+
+  const deposit = async () => {
     if (atm) {
       let tx = await atm.deposit(1);
-      await tx.wait()
+      await tx.wait();
       getBalance();
     }
-  }
+  };
 
-  const withdraw = async() => {
+  const withdraw = async () => {
     if (atm) {
       let tx = await atm.withdraw(1);
-      await tx.wait()
+      await tx.wait();
       getBalance();
     }
-  }
+  };
+
+  const containerTitle = {
+    fontFamily: "Courier New, monospace",
+    textAlign: "center",
+    backgroundColor: "#662549", 
+    color: "white", 
+    borderTopLeftRadius: "15px",
+    borderTopRightRadius: "15px",
+    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+    padding: "20px",
+    width: "800px",
+    margin: "0 auto",
+  };
+
+  const containerStyle = {
+    fontFamily: "Courier New, monospace",
+    textAlign: "center",
+    backgroundColor: "#451952",  
+    color: "white", 
+    borderBottomLeftRadius: "15px",
+    borderBottomRightRadius: "15px",
+    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+    padding: "20px",
+    width: "800px",
+    margin: "0 auto",
+  };
+
+  const buttonStyle = {
+    fontFamily: "Courier New, monospace",
+    backgroundColor: "#F39F5A", 
+    color: "white", 
+    border: "none",
+    padding: "10px 20px",
+    fontSize: "16px",
+    margin: "10px",
+    cursor: "pointer",
+    borderRadius: "15px",
+    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+  };
+
+  const inputStyle = {
+    padding: "10px",
+    fontSize: "16px",
+    width: "300px",
+    marginBottom: "20px",
+    borderRadius: "5px",
+    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+  };
+
+  const disabledButtonStyle = {
+    fontFamily: "Courier New, monospace",
+    backgroundColor: "#FFBA86", 
+    border: "none",
+    padding: "10px 20px",
+    fontSize: "16px",
+    margin: "10px",
+    cursor: "not-allowed",
+    pointerEvents: "none", 
+    borderRadius: "15px",
+    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+  };
 
   const initUser = () => {
     // Check to see if user has Metamask
     if (!ethWallet) {
-      return <p>Please install Metamask in order to use this ATM.</p>
+      return <p>Please install MetaMask to use this ATM.</p>;
     }
 
     // Check to see if user is connected. If not, connect to their account
     if (!account) {
-      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
+      return (
+        <button onClick={connectAccount} style={buttonStyle}>
+          Please connect your MetaMask wallet
+        </button>
+      );
     }
 
-    if (balance == undefined) {
+    if (balance === undefined) {
       getBalance();
     }
 
+    // Check if user is old enough to deposit/withdraw
+    const canDepositWithdraw = userAge >= 18;
+
     return (
       <div>
-        <p>Your Account: {account}</p>
-        <p>Your Balance: {balance}</p>
-        <button onClick={deposit}>Deposit 1 ETH</button>
-        <button onClick={withdraw}>Withdraw 1 ETH</button>
-      </div>
-    )
-  }
+        <p>Your Account: <b>{account}</b></p>
+        <p>Your Balance: <b>{balance} ETH</b></p>
+        {balanceInPhp !== undefined && (
+          <p>Your Balance in PHP: <b>{balanceInPhp} PHP</b></p>
+        )}
+        <p>Your Age: <b>{userAge} Years Old</b></p>
 
-  useEffect(() => {getWallet();}, []);
+        <br></br><p><b>Enter your age below (Number Only):</b></p>
+        <input
+          type="number"
+          placeholder="Enter Your Age"
+          onChange={(e) => setUserAge(e.target.value)}
+          style={inputStyle}
+        /><br></br>
+        <button onClick={deposit} disabled={!canDepositWithdraw} style={!canDepositWithdraw ? disabledButtonStyle : buttonStyle}>
+          <b>Deposit 1 ETH</b>
+        </button>
+        <button onClick={withdraw} disabled={!canDepositWithdraw} style={!canDepositWithdraw ? disabledButtonStyle : buttonStyle}>
+          <b>Withdraw 1 ETH</b>
+        </button>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    getWallet();
+  }, []);
 
   return (
-    <main className="container">
-      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
-      {initUser()}
-      <style jsx>{`
-        .container {
-          text-align: center
-        }
-      `}
-      </style>
+    <main>
+      <header>
+        <h1 style={containerTitle}>Welcome to the Age Restricted ATM!</h1>
+      </header>
+      <div style={containerStyle}>
+        {initUser()}
+      </div>
     </main>
-  )
+  );
 }
